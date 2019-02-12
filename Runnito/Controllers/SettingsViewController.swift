2a74f32267cfd9518c94235b80ee59385c66672c
xpolach4 <xpolach4@mendelu.cz>
2019-02-12 20:25:33 +0200

    profile imagePickerView

--- Runnito/Controllers/SettingsViewController.swift
+++ Runnito/Controllers/SettingsViewController.swift
@@ -10,28 +10,79 @@ import UIKit
 import Firebase
 
 class SettingsViewController: UIViewController {
+    
 
+    var profilePictureImageView : UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "user-circle-b")!
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
+        imageView.tintColor = .lightGray
+        imageView.layer.borderWidth = 2.0
+        imageView.layer.borderColor = UIColor(hexString: "F25652").cgColor
+        imageView.layer.masksToBounds = false
+        imageView.layer.cornerRadius = image.size.width / 2
+        imageView.clipsToBounds = true
+        imageView.image = image
+        return imageView
+    }()
+    
+    var changeProfilePictureButton : SubmitUIButton = {
+        let button = SubmitUIButton()
+        button.setTitle("Tap to change", for: .normal)
+        button.tintColor = .lightGray
+        return button
+    }()
+    
+    
     var logOutButton : SubmitUIButton = {
         let button = SubmitUIButton()
         button.setTitle("Log out", for: .normal)
         return button
     }()
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.addSubview(profilePictureImageView)
+        view.addSubview(changeProfilePictureButton)
         view.addSubview(logOutButton)
-        logOutButton.addTarget(self, action: #selector(logoutHandler), for: .touchUpInside)
+        
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(pictureButtonPressed))
+        profilePictureImageView.addGestureRecognizer(imageTap)
+        logOutButton.addTarget(self, action: #selector(logOutButtonPressed), for: .touchUpInside)
+        changeProfilePictureButton.addTarget(self, action: #selector(pictureButtonPressed), for: .touchUpInside)
         setupLayout()
+        
+        
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+    }
+    
+    @objc func pictureButtonPressed() {
+        self.present(imagePicker, animated: true, completion: nil)
     }
     
     func setupLayout() {
-        logOutButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        logOutButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        logOutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        profilePictureImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        profilePictureImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profilePictureImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        profilePictureImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        changeProfilePictureButton.topAnchor.constraint(equalTo: profilePictureImageView.bottomAnchor, constant: 15).isActive = true
+        changeProfilePictureButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        logOutButton.topAnchor.constraint(equalTo: changeProfilePictureButton.bottomAnchor, constant: 50).isActive = true
+        logOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
     }
     
-    @objc func logoutHandler() {
+    @objc func logOutButtonPressed() {
         do {
             try Auth.auth().signOut()
         } catch let err {
@@ -41,4 +92,33 @@ class SettingsViewController: UIViewController {
         
     }
 
+}
+
+extension SettingsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            profilePictureImageView.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UIImageView {
+    public func maskCircle(anyImage: UIImage) {
+        self.contentMode = UIView.ContentMode.scaleAspectFill
+        self.layer.cornerRadius = self.frame.height / 2
+        self.layer.masksToBounds = false
+        self.clipsToBounds = true
+        
+        // make square(* must to make circle),
+        // resize(reduce the kilobyte) and
+        // fix rotation.
+        self.image = anyImage
+    }
 }
