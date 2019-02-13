967cd0e28ee69f8d4f3221d781db45ec9c6e88ed
xpolach4 <xpolach4@mendelu.cz>
2019-02-13 14:43:33 +0200

    uploadPIcture, cannot get imageURL bug.

--- Runnito/Controllers/RegisterViewController.swift
+++ Runnito/Controllers/RegisterViewController.swift
@@ -12,6 +12,30 @@ import Firebase
 
 class RegisterViewController: UIViewController {
 
+    var profilePictureImageView : UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "user-circle-b")!
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
+        imageView.tintColor = .lightGray
+        imageView.layer.borderWidth = 2.0
+        imageView.layer.borderColor = UIColor(hexString: "F25652").cgColor
+        imageView.layer.masksToBounds = false
+        imageView.layer.cornerRadius = image.size.width / 2
+        imageView.clipsToBounds = true
+        imageView.image = image
+        return imageView
+    }()
+    
+    var changeProfilePictureButton : SubmitUIButton = {
+        let button = SubmitUIButton()
+        button.setTitle("Tap to change", for: .normal)
+        button.tintColor = .lightGray
+        return button
+    }()
+    
+    var imagePicker = UIImagePickerController()
+    
     var activityView:UIActivityIndicatorView!
     
     var usernameTextField : LoginUITextField = {
@@ -45,10 +69,21 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        
         view.addSubview(usernameTextField)
         view.addSubview(mailTextField)
         view.addSubview(passwordTextField)
         view.addSubview(registerButton)
+        view.addSubview(profilePictureImageView)
+        view.addSubview(changeProfilePictureButton)
+        
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(pictureButtonPressed))
+        profilePictureImageView.addGestureRecognizer(imageTap)
+        changeProfilePictureButton.addTarget(self, action: #selector(pictureButtonPressed), for: .touchUpInside)
         
         
         registerButton.addTarget(self, action: #selector(handleRegistration), for: .touchUpInside)
@@ -78,11 +113,20 @@ class RegisterViewController: UIViewController {
         
         let formFilled = username != "" && mail != "" && password != ""
         setRegisterButton(enabled: formFilled)
+        
     }
     
     func setLayout() {
         
-        usernameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        profilePictureImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        profilePictureImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profilePictureImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        profilePictureImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        changeProfilePictureButton.topAnchor.constraint(equalTo: profilePictureImageView.bottomAnchor, constant: 15).isActive = true
+        changeProfilePictureButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        usernameTextField.topAnchor.constraint(equalTo: changeProfilePictureButton.bottomAnchor, constant: 25).isActive = true
         usernameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         usernameTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true
         
@@ -100,6 +144,10 @@ class RegisterViewController: UIViewController {
         
     }
     
+    @objc func pictureButtonPressed() {
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
     func setRegisterButton(enabled: Bool) {
         if enabled {
             registerButton.alpha = 1.0
@@ -112,35 +160,144 @@ class RegisterViewController: UIViewController {
     
     @objc func handleRegistration() {
         guard let username = usernameTextField.text else { return }
-        guard let mail = mailTextField.text else { return }
-        guard let password = passwordTextField.text else { return }
+        guard let email = mailTextField.text else { return }
+        guard let pass = passwordTextField.text else { return }
+        guard let image = profilePictureImageView.image else { return }
         
-        //cannot "register for multiple times"
-        setRegisterButton(enabled: false)
-        registerButton.setTitle("", for: .normal)
+//        setContinueButton(enabled: false)
+//        continueButton.setTitle("", for: .normal)
+        activityView.startAnimating()
         
-        Auth.auth().createUser(withEmail: mail, password: password) { (user, error) in
-            if let err = error {
-                print("Error while creating user: \(err.localizedDescription)")
-            } else {
-                print("User created.")
+        Auth.auth().createUser(withEmail: email, password: pass) { user, error in
+            if error == nil && user != nil {
+                print("🌈 User created!")
                 
-                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-                changeRequest?.displayName = username
-                changeRequest?.commitChanges(completion: { (error) in
-                    if let err = error {
-                        print("Error while changing username: \(err.localizedDescription)")
+                
+                
+                // 1. Upload the profile image to Firebase Storage
+                
+                self.uploadProfileImage(image) { url in
+                    print("🌈 In uploadProfileImage closure")
+                    
+                    if url != nil {
+                        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                        changeRequest?.displayName = username
+                        changeRequest?.photoURL = url
+                        
+                        changeRequest?.commitChanges { error in
+                            if error == nil {
+                                print("User display name changed!")
+                                
+                                self.saveProfile(username: username, profileImageURL: url!) { success in
+                                    if success {
+                                        print("🐰 dismiss called")
+                                        self.dismiss(animated: true, completion: nil)
+                                    }
+                                }
+                                
+                            } else {
+                                print("Error: \(error!.localizedDescription)")
+                            }
+                        }
                     } else {
-                        print("Name changed.")
-                        self.navigationController?.popViewController(animated: true)
+                        // Error unable to upload profile image
                     }
-                })
+                    
+                }
                 
+            } else {
+                print("Error: \(error!.localizedDescription)")
             }
         }
     }
     
-
+    
+    func uploadProfileImage(_ image:UIImage, completion: @escaping ((_ url:URL?)->())) {
+        print("🌈 uploadProfileImage called")
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let storageRef = Storage.storage().reference().child("user/\(uid)")
+        
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+        
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        
+        storageRef.putData(imageData, metadata: metaData) { metaData, error in
+            if error == nil, metaData != nil {
+                storageRef.downloadURL(completion: { (url, error) in
+                    if let error = error {
+                        print("🔥 url error")
+                    } else {
+                        completion(url?.absoluteURL)
+                    }
+                })
+//                if let url = metaData?.downloadURL() {
+//                    completion(url)
+//                } else {
+//                    completion(nil)
+//                }
+//                // success!
+//            } else {
+//                // failed
+//                completion(nil)
+//            }
+        }
+    }
+    }
+    
+    
+    
+    func saveProfile(username: String, profileImageURL: URL, completion: @escaping (_ success:Bool)->()) {
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let databaseRef = Database.database().reference().child("users/profile/\(uid)")
+        
+        let userObject = [
+            "username": username,
+            "photoURL": profileImageURL.absoluteString
+            ] as [String:Any]
+        
+        databaseRef.setValue(userObject) { error, ref in
+            completion(error == nil)
+        }
+        
+    }
     
 
+}
+
+extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        print("didFinishPicking")
+        
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            print("Did finish picking not nil")
+            profilePictureImageView.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
+
+extension UIImageView {
+    public func maskCircle(anyImage: UIImage) {
+        self.contentMode = UIView.ContentMode.scaleAspectFill
+        self.layer.cornerRadius = self.frame.height / 2
+        self.layer.masksToBounds = false
+        self.clipsToBounds = true
+        
+        // make square(* must to make circle),
+        // resize(reduce the kilobyte) and
+        // fix rotation.
+        self.image = anyImage
+    }
 }
--- Runnito/Controllers/SettingsViewController.swift
+++ Runnito/Controllers/SettingsViewController.swift
@@ -11,28 +11,6 @@ import Firebase
 
 class SettingsViewController: UIViewController {
     
-
-    var profilePictureImageView : UIImageView = {
-        let imageView = UIImageView()
-        let image = UIImage(named: "user-circle-b")!
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.isUserInteractionEnabled = true
-        imageView.tintColor = .lightGray
-        imageView.layer.borderWidth = 2.0
-        imageView.layer.borderColor = UIColor(hexString: "F25652").cgColor
-        imageView.layer.masksToBounds = false
-        imageView.layer.cornerRadius = image.size.width / 2
-        imageView.clipsToBounds = true
-        imageView.image = image
-        return imageView
-    }()
-    
-    var changeProfilePictureButton : SubmitUIButton = {
-        let button = SubmitUIButton()
-        button.setTitle("Tap to change", for: .normal)
-        button.tintColor = .lightGray
-        return button
-    }()
     
     
     var logOutButton : SubmitUIButton = {
@@ -41,43 +19,28 @@ class SettingsViewController: UIViewController {
         return button
     }()
     
-    var imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(profilePictureImageView)
-        view.addSubview(changeProfilePictureButton)
+        
         view.addSubview(logOutButton)
         
-        let imageTap = UITapGestureRecognizer(target: self, action: #selector(pictureButtonPressed))
-        profilePictureImageView.addGestureRecognizer(imageTap)
+        
         logOutButton.addTarget(self, action: #selector(logOutButtonPressed), for: .touchUpInside)
-        changeProfilePictureButton.addTarget(self, action: #selector(pictureButtonPressed), for: .touchUpInside)
+        
         setupLayout()
-        
-        
-        imagePicker.allowsEditing = true
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.delegate = self
         
     }
     
-    @objc func pictureButtonPressed() {
-        self.present(imagePicker, animated: true, completion: nil)
-    }
+    
     
     func setupLayout() {
         
-        profilePictureImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        profilePictureImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        profilePictureImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        profilePictureImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
-        changeProfilePictureButton.topAnchor.constraint(equalTo: profilePictureImageView.bottomAnchor, constant: 15).isActive = true
-        changeProfilePictureButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        logOutButton.topAnchor.constraint(equalTo: changeProfilePictureButton.bottomAnchor, constant: 50).isActive = true
+        logOutButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         logOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
     }
@@ -91,34 +54,11 @@ class SettingsViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
         
     }
-
-}
-
-extension SettingsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-            profilePictureImageView.image = image
-        }
-        
-        picker.dismiss(animated: true, completion: nil)
-    }
+    
+    
+    
+    
 }
 
-extension UIImageView {
-    public func maskCircle(anyImage: UIImage) {
-        self.contentMode = UIView.ContentMode.scaleAspectFill
-        self.layer.cornerRadius = self.frame.height / 2
-        self.layer.masksToBounds = false
-        self.clipsToBounds = true
-        
-        // make square(* must to make circle),
-        // resize(reduce the kilobyte) and
-        // fix rotation.
-        self.image = anyImage
-    }
-}
+
